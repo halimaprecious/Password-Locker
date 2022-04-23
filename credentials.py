a95848a441f08d66570475ed2942e27f38fc553d
halima precious <halimaprecious3@gmail.com>
2022-04-23 17:20:13 +0300

    add save method

--- credentials.py
+++ credentials.py
@@ -1,12 +1,21 @@
 
-class Cridentials:
+class Credentials:
     '''
     Class that generates new instances of user credentials.
     '''
 
+    #new credential
     def __init__(self, user_acc, email ,username, password) :
         self.user_acc = user_acc
         self.email = email
         self.username = username
         self.password = password
+
+    #empty cridential list
+    credential_list = []
       
+    def save_cridentials(self):
+        '''
+        Saves credentials into cridential_list
+        '''
+        Credentials.credential_list.append(self)
--- credentials_test.py
+++ credentials_test.py
@@ -1,17 +1,45 @@
-
+"""
+TDD 
+- Create a credential
+     * Args:
+        -account name  -Username    -Email    -Password
+- Save a credential
+- Save more than one credential
+- Display credential
+- Delete credential  
+"""
 import unittest
-from credentials import Cridentials #import credential class
+from credentials import Credentials #import credential class
 
-class TestCridentials (unittest.TestCase):
+class TestCredentials (unittest.TestCase):
 
     def setUp(self):
-        self.new_credentials =Cridentials ("Facebook","peter_p@gmail.com", "peterpan","12345pp")
+        #new credential object
+        self.new_credentials =Credentials ("Facebook","peter_p@gmail.com", "peterpan","12345pp") 
+
+    
+    # Teardown mthd resets credential list after every testcase
+    def tearDown(self):
+        Credentials.credential_list = []    
 
     def test_init(self):
+        '''
+        test case for proper initialization of objects
+        '''
         self.assertEqual(self.new_credentials.user_acc,"Facebook")
         self.assertEqual(self.new_credentials.email,"peter_p@gmail.com")
         self.assertEqual(self.new_credentials.username,"peterpan")
         self.assertEqual(self.new_credentials.password,"12345pp")
+
+
+    def test_save_cridential(self):
+        '''
+        test case to check for saved cridentials.
+        '''
+        #saving new credential
+        self.new_credentials.save_cridentials() 
+        self.assertEqual(len(Credentials.credential_list),1)
+
 
 
 
