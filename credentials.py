8e031ae8184602ec77e27299a19d3bab07fb64f3
halima precious <halimaprecious3@gmail.com>
2022-04-23 15:50:20 +0300

    Creates class Credentials and defines its properties

---
+++ credentials.py
@@ -0,0 +1,12 @@
+
+class Cridentials:
+    '''
+    Class that generates new instances of user credentials.
+    '''
+
+    def __init__(self, user_acc, email ,username, password) :
+        self.user_acc = user_acc
+        self.email = email
+        self.username = username
+        self.password = password
+      
